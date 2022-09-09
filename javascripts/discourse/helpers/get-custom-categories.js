405b473c9c3bb83dc9d157c3ac602e07cfe48452
jumagura <juangura@gmail.com>
2022-09-09 12:07:52 -0400

    DEV: Swap charAt with split to allow unicode emojis

--- javascripts/discourse/helpers/get-custom-categories.js
+++ javascripts/discourse/helpers/get-custom-categories.js
@@ -2,13 +2,17 @@ import { registerUnbound } from "discourse-common/lib/helpers";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import Category from "discourse/models/category";
 
-registerUnbound("getAbbreviation", categoryName => {
+registerUnbound("getAbbreviation", (categoryName) => {
   let abbr = categoryName.replace(" and", "").split(" ");
 
   if (abbr.length > 1) {
-    abbr = abbr[0].charAt(0).toUpperCase() + abbr[1].charAt(0).toLowerCase();
+    abbr =
+      abbr[0].split(/.*?/u).slice(0, 1).join("").toUpperCase() +
+      abbr[1].split(/.*?/u).slice(0, 1).join("").toLowerCase();
   } else {
-    abbr = abbr[0].charAt(0).toUpperCase() + abbr[0].charAt(1).toLowerCase();
+    abbr =
+      abbr[0].split(/.*?/u).slice(0, 1).join("").toUpperCase() +
+      abbr[0].split(/.*?/u).slice(1, 2).join("").toLowerCase();
   }
 
   return abbr;
